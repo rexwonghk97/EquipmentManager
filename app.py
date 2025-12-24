fdba217b7ade11c365ee08bb9455d3fb594336c0
rexwonghk97 <rexwong1997band@gmail.com>
2025-12-25 02:26:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ def close_connection(exception):
 # Email 發送功能
 def send_email_notification(request_id, loan_dt, return_dt, items):
     # ⚠️ 請填入你的 Gmail 設定，若無設定則只會印在 Terminal
-    sender_email = "your_email@gmail.com"
-    sender_password = "your_app_password" # Google App Password
-    receiver_email = "abc@gmail.com"
+    sender_email = "adadatasystem@gmail.com"
+    sender_password = "pveq fqyt hmas pnyk" # Google App Password
+    receiver_email = "rexwong2@ln.edu.hk"
 
     subject = f"New Equipment Request: #{request_id}"
     
